70aeceb3788b1454ba96d4bd46fdfa0c8c95dd19
AliDzII <aruukemyrzalieva@gmail.com>
2024-11-15 23:41:37 +0600

    converter

--- js/lesson.js
+++ js/lesson.js
@@ -14,3 +14,39 @@ phoneButton.onclick = () => {
 
     }
 }
+
+
+// Converter
+
+const usdInput = document.querySelector('#usd');
+const somInput = document.querySelector('#som');
+const eurInput = document.querySelector('#eur');
+
+const converter = (element, targetElement, twoTargetElement) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+            const value = parseFloat(element.value);
+            if (element.id === 'som') {
+                targetElement.value = (value / data.usd).toFixed(2);
+                twoTargetElement.value = (value / data.eur).toFixed(2);
+            } else if (element.id === 'usd') {
+                targetElement.value = (value * data.usd).toFixed(2);
+                twoTargetElement.value = (value * data.usd / data.eur).toFixed(2);
+            } else if (element.id === 'eur') {
+                targetElement.value = (value * data.eur).toFixed(2);
+                twoTargetElement.value = (value * data.eur / data.usd).toFixed(2);
+            }
+        };
+    };
+};
+converter(somInput, usdInput, eurInput);
+converter(usdInput, somInput, eurInput);
+converter(eurInput, somInput, usdInput);
+
+//
